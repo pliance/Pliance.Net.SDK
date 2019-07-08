0803988a9559360013e71646f7a62125e8114710
Adam Furtenbach <adam.furtenbach@gmail.com>
2019-07-08 08:43:13 +0200

    query company

---
+++ Pliance.SDK/Contract/Graphs/Link.cs
@@ -0,0 +1,9 @@
+namespace Pliance.SDK.Contract.Graphs
+{
+    public class Link
+    {
+        public int Source { get; set; }
+        public int Target { get; set; }
+        public string Type { get; set; }
+    }
+}
